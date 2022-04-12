62ce2c64d2e32deaf2c08d962f1a9fb41b25eae6
James Waters <james@jcwaters.co.uk>
2022-04-12 21:58:48 +0100

    Add extra classes option to modal

--- vue-utils/src/components/modal/ModalComponent.vue
+++ vue-utils/src/components/modal/ModalComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="modal modal-open" @click.self="emit('close')">
-    <div class="modal-box">
+    <div class="modal-box" :class="extraClass">
       <p><slot /></p>
       <div class="modal-action">
         <slot name="footer" />
@@ -11,6 +11,10 @@
 
 <script lang="ts" setup>
 import { nextTick, onMounted, ref } from "vue";
+
+const props = defineProps<{
+  extraClass: string
+}>()
 
 const emit = defineEmits<{
   (e: "close"): void;
