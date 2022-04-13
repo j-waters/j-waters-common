1b0431d88517a0a505f5bfa8739c08d06501081c
James Waters <james@jcwaters.co.uk>
2022-04-13 15:41:17 +0100

    Remove broken code from test app

--- vue-utils/src/App.vue
+++ vue-utils/src/App.vue
@@ -4,10 +4,7 @@
 
 <script setup lang="ts">
 import ModalRoot from "@/components/modal/ModalRoot.vue";
-import {useModal} from "@/utils/modal";
-import ModalComponent from "@/components/modal/ModalComponent.vue";
 
-useModal(ModalComponent)({bodyText: "bleh"})
 </script>
 
 
