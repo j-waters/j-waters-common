09ce0652896b0f887c1891bf6a4f35db108d3745
James Waters <james@jcwaters.co.uk>
2022-04-13 15:37:11 +0100

    Add modal to test app

--- vue-utils/src/App.vue
+++ vue-utils/src/App.vue
@@ -1,8 +1,15 @@
+<template>
+  <ModalRoot/>
+</template>
+
 <script setup lang="ts">
+import ModalRoot from "@/components/modal/ModalRoot.vue";
+import {useModal} from "@/utils/modal";
+import ModalComponent from "@/components/modal/ModalComponent.vue";
+
+useModal(ModalComponent)({bodyText: "bleh"})
 </script>
 
-<template>
-</template>
 
 <style>
 
