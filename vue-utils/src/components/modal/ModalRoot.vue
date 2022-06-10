c2e0bed0b39d4795dba98d0636c910da79d56bf8
James Waters <james@jcwaters.co.uk>
2022-06-10 15:14:32 +0100

    Support suspense in Modals

--- vue-utils/src/components/modal/ModalRoot.vue
+++ vue-utils/src/components/modal/ModalRoot.vue
@@ -1,19 +1,23 @@
 <template>
-  <transition-group name="fade">
-    <Component
+  <suspense>
+    <transition-group name="fade">
+      <Component
         :is="modal.component"
         v-for="(modal, index) in activeModals"
         :key="modal.id"
         v-bind="modal.props"
         @close="removeModal(index)"
-    />
-  </transition-group>
+      />
+    </transition-group>
+  </suspense>
 </template>
 
 <script lang="ts" setup>
 import { useModalRoot } from "@/utils/modal";
 
 const { activeModals, removeModal } = useModalRoot();
+
+console.log("hello");
 </script>
 
 <style scoped>
